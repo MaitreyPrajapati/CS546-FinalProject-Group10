22dc5f4bff7661f59f8b4f22f4fa6e8698da633b
Maitrey <maitrey2112@gmail.com>
2020-12-09 13:29:56 -0500

    added await while calling functions from games

--- routes/register.js
+++ routes/register.js
@@ -1,12 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
-
 router.get("/", async (req, res) => {
   res.render("pages/register");
 });
 
-router.post("/", async (req, res) => {
-});
+router.post("/", async (req, res) => {});
 
 module.exports = router;
