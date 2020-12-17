7aecde1444cd552f6173610489efc182262f4c5f
rkim4 <rkim4@stevens.edu>
2020-12-17 15:09:21 -0500

    Update index.js

--- data/index.js
+++ data/index.js
@@ -2,10 +2,12 @@ const users = require('./user');
 const games = require('./game');
 const rentgames = require('./game_rent');
 const sellgames = require("./game_sell");
+const comments = require("./comments")
 
 module.exports = {
     users: users,
     games: games,
     rentgames: rentgames,
-    sellgames: sellgames
+    sellgames: sellgames,
+    comments:comments
 };
